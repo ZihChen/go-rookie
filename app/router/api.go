fca92aba4b9cb814e6aca08f711b7148fe8803a1
Winston <winston0803@cchntek.com>
2023-01-17 15:22:06 +0800

    [chore] gin logger

--- app/router/api.go
+++ app/router/api.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"go-rookie/app/logger"
 	"go-rookie/app/settings"
 )
 
@@ -9,4 +10,9 @@ func Setup(r *gin.Engine) {
 	if settings.Config.Mode == gin.ReleaseMode {
 		gin.SetMode(gin.ReleaseMode)
 	}
+	r.Use(logger.GinLogger(), logger.GinRecovery(false))
+	r.GET("/", func(c *gin.Context) {
+		panic(123)
+		c.JSON(200, "hello world")
+	})
 }
